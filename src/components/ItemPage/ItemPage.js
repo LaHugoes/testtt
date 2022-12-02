9944d17c6a40bdb82975ad657b3cecc7a665d9e1
Ilya <ilyadynin24@gmail.com>
2022-12-02 11:50:01 +0100

    sachen gefixt, durchkommentiert

--- src/components/ItemPage/ItemPage.js
+++ src/components/ItemPage/ItemPage.js
@@ -1,35 +1,34 @@
+import React from "react";
+import Item from "../Item/Item";
+import "./ItemPage.css";
 
-import React from 'react';
-import Item from '../Item/Item';
-import './ItemPage.css';
-
+//hier werden die Reise-Items gemapt und an das Item-Component gereicht, falls keine vorhanden sind, wird angezeigt, dass es keine Items gibt => keine Reiseziel für die Suchanfrage
 function ItemPage({ items }) {
   let length = items.length;
-  if(length > 0) {
+  if (length > 0) {
     return (
       <ul className="ItemPage-items">
-        {items.map(item =>
+        {items.map((item) => (
           <li key={item.id} className="ItemPage-item">
-            <Item item={item}>
-
-            </Item>
+            <Item item={item}></Item>
           </li>
-        )}
+        ))}
       </ul>
     );
   } else {
     return (
       <ul className="ItemPage-items">
         <label>
-        <h5>There are no flights available.</h5>
+          <h5>
+            Es gibt derzeit keine Reisen die auf ihre Suchanfrage zutreffen.
+          </h5>
         </label>
       </ul>
     );
   }
-
 }
 ItemPage.propTypes = {
-  items: React.PropTypes.array.isRequired
+  items: React.PropTypes.array.isRequired,
 };
 
 export default ItemPage;
